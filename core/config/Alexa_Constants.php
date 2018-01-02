7be5027328f70889cd0be2e05f4e92ed26dd0bbd
A Adarsh <adarshdec23@gmail.com>
2018-01-02 18:34:34 +0000

    Issue #1 Duplicate core/config and core/koinex

--- core/config/Alexa_Constants.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace adarshdec23\Config;
-
-abstract class Alexa_Constants{
-    const CRYPTO_SLOT = "CryptoCurrencyIntentSlot";
-    const ERROR_UNABLE_TO_PARSE = "Sorry, there is something wrong with me today. I may have the flu? Could you contact my developer?";
-    const ERROR_TOKEN_NOT_FOUND = "Sorry, that was not understood. Which Crypto currency's price would you like to check?";
-}
